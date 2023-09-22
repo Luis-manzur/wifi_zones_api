65405f0ad6867b9fcbad471e73898e9c7c061453
luism <luismanzur91@outlook.com>
2023-09-22 15:35:41 -0400

    reformat project

--- config/settings/base.py
+++ config/settings/base.py
@@ -274,9 +274,7 @@ CELERY_TASK_SEND_SENT_EVENT = True
 # -------------------------------------------------------------------------------
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.TokenAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }

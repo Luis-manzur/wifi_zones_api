689ef11fbc695b3b628e8ecf0dedca2715a1b502
luism <luismanzur91@outlook.com>
2023-09-22 15:47:55 -0400

    profile model

---
+++ wifi_zones_api/users/models/profiles.py
@@ -0,0 +1,25 @@
+"""Profile model."""
+
+# Django
+from django.db import models
+
+# Utilities
+from wifi_zones_api.utils.models import WZModel
+from wifi_zones_api.utils.validators import validate_birth_date
+
+
+class Profile(WZModel):
+    """Profile model.
+    A profile holds a user's public data.
+    """
+
+    GENDERS = [("F", "Feminine"), ("M", "Masculine"), ("O", "Other")]
+
+    user = models.OneToOneField("users.User", related_name="profile", on_delete=models.CASCADE)
+    birth_date = models.DateField(validators=[validate_birth_date])
+    address = models.CharField(max_length=60)
+    gender = models.CharField(max_length=1, choices=GENDERS, default="O")
+
+    def __str__(self):
+        """Return user's str representation."""
+        return str(self.user)

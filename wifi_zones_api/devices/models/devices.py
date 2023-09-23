a1e16b34aaafc0526f682607ed7d4e716976b8fa
luism <luismanzur91@outlook.com>
2023-09-23 17:17:41 -0400

    device model

---
+++ wifi_zones_api/devices/models/devices.py
@@ -0,0 +1,24 @@
+"""Device model"""
+
+from django.core.validators import RegexValidator
+# Django
+from django.db import models
+
+# Utilities
+from wifi_zones_api.utils.models import WZModel
+
+
+class Device(WZModel):
+    """Device model.
+        A device holds a user's device data for UC auto connection.
+        """
+    name = models.CharField()
+    model = models.CharField()
+    brand = models.CharField()
+    mac_regex = RegexValidator(
+        regex=r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$',
+        message='Enter a valid MAC address.',
+    )
+    mac = models.CharField(max_length=17, )
+
+    user = models.ForeignKey('users.User', related_name='device', on_delete=models.CASCADE)

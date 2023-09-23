cd1d4c6b696696a81c7ab3b703069200a8d71b5b
luism <luismanzur91@outlook.com>
2023-09-22 21:37:51 -0400

    add state and municipality to users

--- wifi_zones_api/users/models/profiles.py
+++ wifi_zones_api/users/models/profiles.py
@@ -2,6 +2,7 @@
 
 # Django
 from django.db import models
+from smart_selects.db_fields import ChainedForeignKey
 
 # Utilities
 from wifi_zones_api.utils.models import WZModel
@@ -18,6 +19,14 @@ class Profile(WZModel):
     user = models.OneToOneField("users.User", related_name="profile", on_delete=models.CASCADE)
     birth_date = models.DateField(validators=[validate_birth_date])
     address = models.CharField(max_length=60)
+    state = models.ForeignKey('locations.State', on_delete=models.SET_NULL, null=True)
+    municipality = ChainedForeignKey(
+        'locations.Municipality',
+        chained_field="state",
+        chained_model_field="state",
+        show_all=False,
+        auto_choose=True,
+        on_delete=models.SET_NULL, null=True)
     gender = models.CharField(max_length=1, choices=GENDERS, default="O")
 
     def __str__(self):

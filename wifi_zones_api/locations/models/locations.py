b160b3994b69c4bd7a017949955c36a962e6f710
luism <luismanzur91@outlook.com>
2023-10-11 14:10:09 -0400

    add image to location, add media url

--- wifi_zones_api/locations/models/locations.py
+++ wifi_zones_api/locations/models/locations.py
@@ -12,6 +12,7 @@ from wifi_zones_api.utils.models import WZModel
 
 class Location(WZModel):
     name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='locations/', null=True)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     municipality = ChainedForeignKey(
         Municipality,

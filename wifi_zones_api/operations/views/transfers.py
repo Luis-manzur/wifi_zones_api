b2cb37fe8ecd0c11564d99f5cba9d145c95a49bc
luism <luismanzur91@outlook.com>
2023-10-27 13:52:01 -0400

    transfer response

--- wifi_zones_api/operations/views/transfers.py
+++ wifi_zones_api/operations/views/transfers.py
@@ -1,17 +1,20 @@
 """Transfers view sets"""
-
-# Django filters
+# Django
+from django.utils.translation import gettext_lazy as _
 # DRF
-from rest_framework import viewsets, mixins
+from drf_spectacular.utils import extend_schema, inline_serializer
+from rest_framework import viewsets, mixins, serializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Models
 from wifi_zones_api.operations.models import Transfer
 # Serializer
 from wifi_zones_api.operations.serializers.transfers import TransferCreateModelSerializer
 
-
-# Utilities
+confirmation_inline_serializer = inline_serializer(
+    name="VerifyInlineSerializer", fields={"message": serializers.CharField()}
+)
 
 
 class TransferViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
@@ -26,3 +29,12 @@ class TransferViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
         permissions = [IsAuthenticated]
 
         return [p() for p in permissions]
+
+    @extend_schema(
+        responses={201: inline_serializer},
+    )
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": _("Successful Transfer.")})

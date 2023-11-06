0eb1764ee14ff182de961d812188f360178418d4
luism <luismanzur91@outlook.com>
2023-11-06 17:58:01 -0400

    complete pago movil functionality

--- wifi_zones_api/operations/views/pago_movil.py
+++ wifi_zones_api/operations/views/pago_movil.py
@@ -1,15 +1,26 @@
 """PagoMovil Viewset"""
-
 # DRF
-from rest_framework import viewsets, mixins
+from drf_spectacular.utils import inline_serializer
+from rest_framework import viewsets, mixins, serializers
+from rest_framework.response import Response
 
 # Models
 from wifi_zones_api.operations.models import PagoMovil
-
 # Serializer
 from wifi_zones_api.operations.serializers.recharges import PagoMovilCreateModelSerializer
+
+confirmation_inline_serializer = inline_serializer(
+    name="PagoMovilConfirmationInlineSerializer",
+    fields={"code": serializers.IntegerField(default=200), "Refpk": serializers.CharField()}
+)
 
 
 class PagoMovilViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     serializer_class = PagoMovilCreateModelSerializer
     queryset = PagoMovil.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.save()
+        return Response({"code": 200, "Refpk": data["Refpk"]}, status=201)

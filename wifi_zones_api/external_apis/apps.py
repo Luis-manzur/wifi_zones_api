6059b91fe7444053abea7a8aef19c78a92fbd907
luism <luismanzur91@outlook.com>
2023-12-20 09:56:11 -0400

    add empty parameters for testing

--- wifi_zones_api/external_apis/apps.py
+++ wifi_zones_api/external_apis/apps.py
@@ -4,14 +4,16 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 from wifi_zones_api.external_apis.ruijie import login
+from django.conf import settings
 
 logger = logging.getLogger("console")
 
-
+settings_file = settings.env('DJANGO_SETTINGS_MODULE')
 class ExternalAPISConfig(AppConfig):
     name = "wifi_zones_api.external_apis"
     verbose_name = _("External API'S")
 
     def ready(self):
-        login()
+        if settings_file != "config.settings.test":
+            login()
         logger.info("external apis app started")

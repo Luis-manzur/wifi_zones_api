c42c018024fc9b99862290a28c2eb612b0648e6d
luism <luismanzur91@outlook.com>
2023-12-20 14:55:21 -0400

    refactor: change domain

--- config/settings/base.py
+++ config/settings/base.py
@@ -305,7 +305,7 @@ SPECTACULAR_SETTINGS = {
 # URL
 URL = env(
     "CLIENT_URL",
-    default="ec2-54-205-106-112.compute-1.amazonaws.com",
+    default="wifi-zones-api.datinvoz.com",
 )
 # RUIJIE
 RUIJIE_URL = env("RUIJIE_URL")

01f7ca1a6c977e82729c31bb25c475b34330aa1a
luism <luismanzur91@outlook.com>
2024-01-12 16:59:48 -0400

    feat: add user admin

--- wifi_zones_api/users/admin.py
+++ wifi_zones_api/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User  # Assuming your custom user model is in models.py
-
-admin.site.register(User, UserAdmin)
+from wifi_zones_api.utils.admin import admin_site
+admin_site.register(User, UserAdmin)

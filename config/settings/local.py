33ff5b178ecf4cade6edad1fb0e15b93bb57e612
luism <luismanzur91@outlook.com>
2024-01-17 10:41:30 -0400

    feat: add ip middlewares

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,6 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*"]
-
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
---
+++ wifi_zones_api/utils/middlewares.py
@@ -0,0 +1,20 @@
+"""Common Middlewares"""
+
+# Django
+from django.http import HttpResponseForbidden
+from django.conf import settings
+
+class IPRestrictionMiddleware:
+    target_urls = [
+        '/operations/recharge/pago-movil/',
+    ]
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.allowed_ip = settings.ALLOWED_PM_IP  # Replace with the allowed IP address
+
+    def __call__(self, request):
+        if request.path in self.target_urls:
+            if request.META.get('REMOTE_ADDR') != self.allowed_ip:
+                return HttpResponseForbidden("Access denied")
+        response = self.get_response(request)
+        return response

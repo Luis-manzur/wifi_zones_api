e2778c8dded4a8084342a711a633f50049bc6679
luism <luismanzur91@outlook.com>
2024-02-16 09:49:18 -0400

    add all operations to admin

--- wifi_zones_api/operations/admin.py
+++ wifi_zones_api/operations/admin.py
@@ -5,7 +5,7 @@ import csv
 from django.http import HttpResponse
 
 # models
-from wifi_zones_api.operations.models import Operation
+from wifi_zones_api.operations.models import Operation, Recharge, PagoMovil, Payment
 
 def export_to_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
@@ -20,7 +20,7 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 
 class OperationAdmin(admin.ModelAdmin):
-    list_display = ("pk", "user",  "operation_type", "code")
+    list_display = ("pk", "user",  "operation_type", "code", "created")
     list_filter = ("user", "operation_type", "created")
     actions = [export_to_csv]
 
@@ -34,3 +34,53 @@ class OperationAdmin(admin.ModelAdmin):
         return False
 
 admin.site.register(Operation, OperationAdmin)
+
+class RechargeAdmin(admin.ModelAdmin):
+    list_display = ("pk", "amount",  "payment_method", "created")
+    list_filter = ("payment_method", "created")
+    actions = [export_to_csv]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Recharge, RechargeAdmin)
+
+
+class PMAdmin(admin.ModelAdmin):
+    list_display = ("pk", "reference_number",  "origin_phone_number", "status", "bank")
+    list_filter = ("status", "created")
+    actions = [export_to_csv]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+admin.site.register(PagoMovil, PMAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "amount", "payment_method", "created")
+    list_filter = ("payment_method", "created")
+    actions = [export_to_csv]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Payment, PaymentAdmin)

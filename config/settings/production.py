55f756c7ff3c5c341d500b1c6228eeebc8439e66
luism <luismanzur91@outlook.com>
2024-02-16 17:25:18 -0400

    feat: add cdn confoguration

--- config/settings/production.py
+++ config/settings/production.py
@@ -172,5 +172,5 @@ AWS_S3_OBJECT_PARAMETERS = {
     'ACL': 'public-read'
 }
 AWS_LOCATION = 'https://banff.nyc3.digitaloceanspaces.com'
-DEFAULT_FILE_STORAGE = 'frida_ayala.cdn.backends.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'frida_ayala.cdn.backends.StaticRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'wifi_zones_api.cdn.backends.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'wifi_zones_api.cdn.backends.StaticRootS3BotoStorage'

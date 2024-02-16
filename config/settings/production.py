1e35c7040abc5dbe96af133debc8891b50d15946
luism <luismanzur91@outlook.com>
2024-02-16 17:23:11 -0400

    feat: add cdn confoguration

--- config/settings/production.py
+++ config/settings/production.py
@@ -163,3 +163,14 @@ SPECTACULAR_SETTINGS["SERVERS"] = [  # noqa: F405
 ]
 # Your stuff...
 # ------------------------------------------------------------------------------
+AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read'
+}
+AWS_LOCATION = 'https://banff.nyc3.digitaloceanspaces.com'
+DEFAULT_FILE_STORAGE = 'frida_ayala.cdn.backends.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'frida_ayala.cdn.backends.StaticRootS3BotoStorage'

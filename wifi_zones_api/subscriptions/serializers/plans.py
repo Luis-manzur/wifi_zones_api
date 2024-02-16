e8e8073443cdea4d9908bdf91da334480346db44
luism <luismanzur91@outlook.com>
2024-02-16 16:43:03 -0400

    refactor: add daily_price to serializer

--- wifi_zones_api/subscriptions/serializers/plans.py
+++ wifi_zones_api/subscriptions/serializers/plans.py
@@ -12,20 +12,16 @@ from wifi_zones_api.subscriptions.models import Plan
 
 class PlanListModelSerializer(serializers.ModelSerializer):
     """Plan list model serializer."""
-    monthly_price = serializers.SerializerMethodField()
-    yearly_price = serializers.SerializerMethodField()
+    daily_price = serializers.SerializerMethodField()
 
-    def get_monthly_price(self, obj):
+    def get_daily_price(self, obj):
         exchange_rate = cache.get('exchange_rate')
-        return obj.monthly_price * Decimal(str(exchange_rate))
+        return obj.daily_price * Decimal(str(exchange_rate))
 
-    def get_yearly_price(self, obj):
-        exchange_rate = cache.get('exchange_rate')
-        return obj.yearly_price * Decimal(str(exchange_rate))
 
     class Meta:
         model = Plan
-        fields = ["name", "yearly_price", "monthly_price", "id"]
+        fields = ["name", "yearly_price", "monthly_price", "daily_price", "id"]
 
 
 class PlanModelSerializer(serializers.ModelSerializer):
